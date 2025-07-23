e2c64a832d093ba3637922bc52e559f919597dad
Jiwon Choi <jiwon719choi@gmail.com>
2025-07-23 16:58:24 +0900

    지옥같은 카카오맵..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
-// frontend/src/App.jsx
 import React from "react";
-import KakaoMap from "./KakaoMap"; // 새로 만든 KakaoMap 컴포넌트를 import
+import KakaoMap from "./KakaoMap";
 
 function App() {
   return (
@@ -8,8 +7,7 @@ function App() {
       <div>
         <h1>프론트엔드 메인 페이지</h1>
       </div>
-      <KakaoMap /> {/* KakaoMap 컴포넌트를 여기에 렌더링합니다. */}
-      {/* ToastContainer는 main.jsx에서 전역적으로 관리되므로 App.jsx에서는 필요 없습니다. */}
+      <KakaoMap />
     </>
   );
 }
