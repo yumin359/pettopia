a0829dd5ce0dd8e7750d902d86f37a10fb0c28c0
yumin359 <yumin359@gmail.com>
2025-07-24 11:15:28 +0900

    entity member 수정

--- backend/src/main/java/com/example/backend/member/entity/Member.java
+++ backend/src/main/java/com/example/backend/member/entity/Member.java
@@ -1,7 +1,9 @@
 package com.example.backend.member.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
@@ -10,14 +12,24 @@ import java.time.LocalDateTime;
 @Table(name = "member")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 
     @Id
-    private String email;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String email;
     private String password;
     private String nickName;
     private String info;
+
+    private String provider;
+    private String providerId;
+
+    // 나중에 Enum?으로 바꿀 듯
+    private String role;
 
     @Column(insertable = false, updatable = false)
     private LocalDateTime insertedAt;
