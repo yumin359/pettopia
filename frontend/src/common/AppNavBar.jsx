c125c79063850229237a9cb26c8857b6316c8d0b
yopig <rrrmr9410@naver.com>
2025-07-29 17:45:27 +0900

    디자인 수정

--- frontend/src/common/AppNavBar.jsx
+++ frontend/src/common/AppNavBar.jsx
@@ -1,8 +1,7 @@
-import { Link, NavLink } from "react-router";
+import { Link, NavLink } from "react-router-dom"; // ✅ 수정
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { useContext } from "react";
 import { AuthenticationContext } from "./AuthenticationContextProvider.jsx";
-import { FiUser } from "react-icons/fi";
 import { FaUserCircle } from "react-icons/fa";
 
 export function AppNavBar() {
@@ -11,16 +10,14 @@ export function AppNavBar() {
   return (
     <Navbar
       expand="lg"
-      // bg="light"
       fixed="top"
-      // className="bg-body-tertiary"
       style={{
         backgroundColor: "#FAF0E6",
         borderBottom: "1px solid #ccc",
       }}
     >
-      <Container className="d-flex align-items-center">
-        {/* 1. 로고 */}
+      <Container>
+        {/* 로고 (왼쪽 정렬) */}
         <Navbar.Brand as={Link} to="/" className="fs-3 fw-bold me-4">
           <img
             src="/PETOPIA.png"
@@ -30,43 +27,31 @@ export function AppNavBar() {
           />
         </Navbar.Brand>
 
-        {/* 2. 로그인/닉네임 + 햄버거 토글 (항상 고정, Collapse 밖) */}
+        {/* 로그인/닉네임 (오른쪽) */}
         <div className="d-flex align-items-center order-lg-2">
           <Nav className="me-3">
             {!user ? (
-              <Nav.Link as={NavLink} to="/login">
-                로그인
-              </Nav.Link>
+              <Nav.Link as={NavLink} to="/login">로그인</Nav.Link>
             ) : (
               <Nav.Link as={NavLink} to={`/member?email=${user.email}`}>
                 <FaUserCircle size={19} className="me-1" />
-                {/* 아이콘 크기 조절 나중에 관리자면 색깔 바꿀까 .. */}
                 {user.nickName}
               </Nav.Link>
             )}
           </Nav>
-
           <Navbar.Toggle aria-controls="main-nav" />
         </div>
 
-        {/* 3. 메뉴 (Collapse 안, order-lg-1로 로고 옆에 위치) */}
-        <Navbar.Collapse id="main-nav" className="order-lg-1">
-          <Nav>
-            <Nav.Link as={NavLink} to="/kakaoMap">
-              지도
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/board/list">
-              커뮤니티
-            </Nav.Link>
+        {/* 메뉴 (가운데 정렬) */}
+        <Navbar.Collapse id="main-nav" className="justify-content-center order-lg-1">
+          <Nav className="gap-5">
+            <Nav.Link as={NavLink} to="/">HOME</Nav.Link>
+            <Nav.Link as={NavLink} to="/kakaoMap">MAP</Nav.Link>
+            <Nav.Link as={NavLink} to="/board/list">EVENT</Nav.Link>
             {isAdmin() && (
-              <Nav.Link as={NavLink} to="/member/list">
-                회원 목록
-              </Nav.Link>
+              <Nav.Link as={NavLink} to="/member/list">MEMBERLIST</Nav.Link>
             )}
           </Nav>
-          <Nav.Link as={NavLink} to="/chatbot">
-            챗봇(쓰지마)
-          </Nav.Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
