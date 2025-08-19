2432d1a3feb14c1968035ff8954f3cc2938ca947
nowBnew <83652065+nowBnew@users.noreply.github.com>
2025-08-20 00:03:41 +0900

    스웨거를 사용했습니다.

---
+++ backend/src/main/java/com/example/backend/config/SwaggerConfig.java
@@ -0,0 +1,54 @@
+package com.example.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Example API Docs",
+                description = "Description",
+                version = "v1"
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "local test server"),
+        }
+)
+@Configuration
+@Profile({"default", "qa"})
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    private static final String BEARER_TOKEN_PREFIX = "Bearer";
+
+    @Bean
+    public OpenAPI openAPI() {
+        String securityJwtName = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+        Components components = new Components()
+                .addSecuritySchemes(securityJwtName, new SecurityScheme()
+                        .name(securityJwtName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme(BEARER_TOKEN_PREFIX)
+                        .bearerFormat(securityJwtName));
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+}
